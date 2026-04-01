c816c13502571aea781d64c722fdcfe79601dac5
Charles Chandrahasan <charliedc12@gmail.com>
2026-03-31 21:13:19 -0700

    init commit

---
+++ stedi-v1.0/stedi-v1.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stedi-v1.0
+//
+//  Created by Charles Chandrahasan on 3/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("wassup!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stedi-v1.0/stedi-v1.0/stedi_v1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  stedi_v1_0App.swift
+//  stedi-v1.0
+//
+//  Created by Charles Chandrahasan on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct stedi_v1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
